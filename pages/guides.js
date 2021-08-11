c8fedc93fcaad222bb0f2df49c1a0901a1bba00f
cotamor <cotamor@gmail.com>
2021-08-11 23:57:13 +0900

    identity function added

--- pages/guides.js
+++ pages/guides.js
@@ -1,9 +1,62 @@
-import styles from '../styles/Guides.module.css'
+import styles from "../styles/Guides.module.css";
+import { useState, useEffect, useContext } from "react";
+import AuthContext from "../stores/authContext";
 
 export default function Guides() {
+  const { user, authReady } = useContext(AuthContext);
+  const [guides, setGuides] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    if (authReady) {
+      fetch(
+        ".netlify/functions/guides",
+        user && {
+          headers: {
+            Authorization: "Bearer " + user.token.access_token,
+          },
+        }
+      )
+        .then((res) => {
+          if (!res.ok) {
+            throw Error("You must be logged in to view this content");
+          }
+          return res.json();
+        })
+        .then((data) => {
+          setGuides(data);
+          setError(null);
+        })
+        .catch((err) => {
+          setError(err.message);
+          setGuides(null);
+        });
+    }
+  }, [user, authReady]);
+
   return (
     <div className={styles.guides}>
-      <h2>All Guides</h2>
-    </div> 
-  )
+      {!authReady && <div>Loading...</div>}
+
+      {error && (
+        <div className={styles.error}>
+          <p>{error}</p>
+        </div>
+      )}
+
+      {guides &&
+        guides.map((guide) => (
+          <div key={guide.title} className={styles.card}>
+            <h3>{guide.title}</h3>
+            <h4>Written by {guide.author}</h4>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat
+              deleniti velit commodi praesentium repellendus accusamus porro
+              ducimus vitae ipsam officia facere dignissimos, quas accusantium
+              mollitia placeat autem, voluptates consequuntur labore.
+            </p>
+          </div>
+        ))}
+    </div>
+  );
 }
